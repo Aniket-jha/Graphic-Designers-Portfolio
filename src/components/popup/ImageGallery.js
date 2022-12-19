e8d3996e4885e4054bf29c686bbfc1341e667688
Aniket-jha <officialaniketjha@gmail.com>
2022-12-19 18:28:04 +0530

    general changes

--- src/components/popup/ImageGallery.js
+++ src/components/popup/ImageGallery.js
@@ -33,7 +33,7 @@ const ImgView = ({ close, imgs }) => {
               <div className="mfp-iframe-scaler">
               
                 <div className="img-container">
-                 <div onClick={() => close()} className="close-btn" ><span>X</span></div>
+                 <div onClick={() => close()} className="close-btn " ><span>X</span></div>
                   <img
                   
                     style={{overflow:"hidden" }}
